22416b7654edbd42a8a420ecf9ddeb64261e7bdc
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2018-03-13 19:19:32 -0400

    Add files via upload

---
+++ multTable.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+//Draws the table
+void table(int tableSize)
+{
+	const int row = 13;
+	const int col = 13;
+	int nums[row][col];
+
+	for (int row = 0; row <= tableSize; row++)
+	{
+		for (int col = 0; col <= tableSize; col++)
+		{
+			nums[row][col] = row*col;
+			cout << setw(4) << nums[row][col];
+		}
+	cout << endl << endl;
+}
+
+}
+
+//Asks user for size of array, then validates.
+void sizeDetermination()
+{
+	int tableSize = 0;
+	cout << "Enter size of multiplication table (Max 12): ";
+	cin >> tableSize;
+	while (tableSize > 12)
+	{
+		cout << "Invalid number. Please try again: ";
+		cin >> tableSize;
+	}
+		table(tableSize);
+}
+
+int main()
+{
+	sizeDetermination();
+	return 0;
+}
